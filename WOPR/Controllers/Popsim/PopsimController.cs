e165a2652857a3d70c9d12686001519be8b4f5fc
towergame <towergame.epic@gmail.com>
2021-07-12 01:17:05 +0300

    Added a group editor

--- WOPR/Controllers/Popsim/PopsimController.cs
+++ WOPR/Controllers/Popsim/PopsimController.cs
@@ -13,150 +13,228 @@ using WOPR.Helpers;
 
 namespace WOPR.Controllers.Popsim
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class PopsimController : ControllerBase
-	{
-		private readonly BabelContext _context;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PopsimController : ControllerBase
+    {
+        private readonly BabelContext _context;
 
-		public PopsimController(BabelContext context)
-		{
-			_context = context;
-		}
+        public PopsimController(BabelContext context)
+        {
+            _context = context;
+        }
 
-		public struct PopsimPartyOverviewReturn
-		{
-			public float UpperPartyPercentage { get; set; }
-			public List<PopsimOverviewGlobalEthicGroup> GlobalEthicGroups { get; set; }
-			public List<PopsimOverviewAlignment> Alignments { get; set; }
-		}
+        public struct PopsimPartyOverviewReturn
+        {
+            public float UpperPartyPercentage { get; set; }
+            public List<PopsimOverviewGlobalEthicGroup> GlobalEthicGroups { get; set; }
+            public List<PopsimOverviewAlignment> Alignments { get; set; }
+        }
 
-		public struct PopsimOverviewGlobalEthicGroup
-		{
-			public string Name { get; set; }
-			public int PartyInvolvementFactor { get; set; }
-			public float Radicalisation { get; set; }
-			public float PartyEnlistmentModifier { get; set; }
+        public struct PopsimOverviewGlobalEthicGroup
+        {
+            public string Name { get; set; }
+            public int PartyInvolvementFactor { get; set; }
+            public float Radicalisation { get; set; }
+            public float PartyEnlistmentModifier { get; set; }
 
-			public int FederalismCentralism { get; set; }
-			public int DemocracyAuthority { get; set; }
-			public int GlobalismIsolationism { get; set; }
-			public int MilitarismPacifism { get; set; }
-			public int SecurityFreedom { get; set; }
-			public int CooperationCompetition { get; set; }
-			public int SecularismSpiritualism { get; set; }
-			public int ProgressivismTraditionalism { get; set; }
-			public int MonoculturalismMulticulturalism { get; set; }
+            public int FederalismCentralism { get; set; }
+            public int DemocracyAuthority { get; set; }
+            public int GlobalismIsolationism { get; set; }
+            public int MilitarismPacifism { get; set; }
+            public int SecurityFreedom { get; set; }
+            public int CooperationCompetition { get; set; }
+            public int SecularismSpiritualism { get; set; }
+            public int ProgressivismTraditionalism { get; set; }
+            public int MonoculturalismMulticulturalism { get; set; }
 
-			public float PopGroupEnlistment { get; set; }
-			public float UpperPartyMembership { get; set; }
-			public float LowerPartyMembership { get; set; }
-		}
+            public float PopGroupEnlistment { get; set; }
+            public float UpperPartyMembership { get; set; }
+            public float LowerPartyMembership { get; set; }
+        }
 
-		public struct PopsimOverviewAlignment
-		{
-			public string Name { get; set; }
+        public struct PopsimOverviewAlignment
+        {
+            public string Name { get; set; }
 
-			public int FederalismCentralism { get; set; }
-			public int DemocracyAuthority { get; set; }
-			public int GlobalismIsolationism { get; set; }
-			public int MilitarismPacifism { get; set; }
-			public int SecurityFreedom { get; set; }
-			public int CooperationCompetition { get; set; }
-			public int SecularismSpiritualism { get; set; }
-			public int ProgressivismTraditionalism { get; set; }
-			public int MonoculturalismMulticulturalism { get; set; }
+            public int FederalismCentralism { get; set; }
+            public int DemocracyAuthority { get; set; }
+            public int GlobalismIsolationism { get; set; }
+            public int MilitarismPacifism { get; set; }
+            public int SecurityFreedom { get; set; }
+            public int CooperationCompetition { get; set; }
+            public int SecularismSpiritualism { get; set; }
+            public int ProgressivismTraditionalism { get; set; }
+            public int MonoculturalismMulticulturalism { get; set; }
 
-			public float Establishment { get; set; }
-			public float UpperPartyModifier { get; set; }
-			public float LowerPartyModifier { get; set; }
-			public float UpperPartyAffinity { get; set; }
-			public float LowerPartyAffinity { get; set; }
+            public float Establishment { get; set; }
+            public float UpperPartyModifier { get; set; }
+            public float LowerPartyModifier { get; set; }
+            public float UpperPartyAffinity { get; set; }
+            public float LowerPartyAffinity { get; set; }
 
-			public List<string> Cliques { get; set; }
-		}
+            public List<string> Cliques { get; set; }
+        }
 
-		[HttpGet("overview")]
-		[Authorize(AuthenticationSchemes = "Discord")]
-		public IActionResult GetPopsimPartyOverview()
-		{
-			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        [HttpGet("overview")]
+        [Authorize(AuthenticationSchemes = "Discord")]
+        public IActionResult GetPopsimPartyOverview()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-			var discordUser = _context.DiscordUsers.SingleOrDefault(du => du.DiscordUserId == userId);
+            var discordUser = _context.DiscordUsers.SingleOrDefault(du => du.DiscordUserId == userId);
 
-			if (discordUser == null || !discordUser.IsAdmin)
-			{
-				return Unauthorized();
-			}
+            if (discordUser == null || !discordUser.IsAdmin)
+            {
+                return Unauthorized();
+            }
 
-			var party = _context.Parties.First();
+            var party = _context.Parties.First();
 
-			var overview = new PopsimPartyOverviewReturn();
+            var overview = new PopsimPartyOverviewReturn();
 
-			overview.GlobalEthicGroups = new List<PopsimOverviewGlobalEthicGroup>();
-			overview.Alignments = new List<PopsimOverviewAlignment>();
+            overview.GlobalEthicGroups = new List<PopsimOverviewGlobalEthicGroup>();
+            overview.Alignments = new List<PopsimOverviewAlignment>();
 
-			foreach (var enlistment in party.PopGroupEnlistment)
-			{
-				var ppeg = enlistment.Key;
-				var newEntry = new PopsimOverviewGlobalEthicGroup()
-				{
-					Name = ppeg.PopsimGlobalEthicGroupName,
-					FederalismCentralism = ppeg.FederalismCentralism,
-					DemocracyAuthority = ppeg.DemocracyAuthority,
-					GlobalismIsolationism = ppeg.GlobalismIsolationism,
-					MilitarismPacifism = ppeg.MilitarismPacifism,
-					SecurityFreedom = ppeg.SecurityFreedom,
-					CooperationCompetition = ppeg.CooperationCompetition,
-					SecularismSpiritualism = ppeg.SecularismSpiritualism,
-					ProgressivismTraditionalism = ppeg.ProgressivismTraditionalism,
-					MonoculturalismMulticulturalism = ppeg.MonoculturalismMulticulturalism,
-					PopGroupEnlistment = enlistment.Value
-				};
-			}
+            foreach (var enlistment in party.PopGroupEnlistment)
+            {
+                var ppeg = enlistment.Key;
+                var newEntry = new PopsimOverviewGlobalEthicGroup()
+                {
+                    Name = ppeg.PopsimGlobalEthicGroupName,
+                    FederalismCentralism = ppeg.FederalismCentralism,
+                    DemocracyAuthority = ppeg.DemocracyAuthority,
+                    GlobalismIsolationism = ppeg.GlobalismIsolationism,
+                    MilitarismPacifism = ppeg.MilitarismPacifism,
+                    SecurityFreedom = ppeg.SecurityFreedom,
+                    CooperationCompetition = ppeg.CooperationCompetition,
+                    SecularismSpiritualism = ppeg.SecularismSpiritualism,
+                    ProgressivismTraditionalism = ppeg.ProgressivismTraditionalism,
+                    MonoculturalismMulticulturalism = ppeg.MonoculturalismMulticulturalism,
+                    PopGroupEnlistment = enlistment.Value
+                };
+            }
 
-			foreach (var upperMembership in party.UpperPartyMembership)
-			{
-				var entry = overview.GlobalEthicGroups.SingleOrDefault(geg => geg.Name == upperMembership.Key.PopsimGlobalEthicGroupName);
-				entry.UpperPartyMembership = upperMembership.Value;
-			}
+            foreach (var upperMembership in party.UpperPartyMembership)
+            {
+                var entry = overview.GlobalEthicGroups.SingleOrDefault(geg => geg.Name == upperMembership.Key.PopsimGlobalEthicGroupName);
+                entry.UpperPartyMembership = upperMembership.Value;
+            }
 
-			foreach (var lowerMembership in party.LowerPartyMembership)
-			{
-				var entry = overview.GlobalEthicGroups.SingleOrDefault(geg => geg.Name == lowerMembership.Key.PopsimGlobalEthicGroupName);
-				entry.LowerPartyMembership = lowerMembership.Value;
-			}
+            foreach (var lowerMembership in party.LowerPartyMembership)
+            {
+                var entry = overview.GlobalEthicGroups.SingleOrDefault(geg => geg.Name == lowerMembership.Key.PopsimGlobalEthicGroupName);
+                entry.LowerPartyMembership = lowerMembership.Value;
+            }
 
-			foreach (var lowerAffinity in party.LowerPartyAffinity)
-			{
-				var a = lowerAffinity.Key;
-				var newEntry = new PopsimOverviewAlignment()
-				{
-					Name = a.AlignmentName,
-					FederalismCentralism = a.FederalismCentralism,
-					DemocracyAuthority = a.DemocracyAuthority,
-					GlobalismIsolationism = a.GlobalismIsolationism,
-					MilitarismPacifism = a.MilitarismPacifism,
-					SecurityFreedom = a.SecurityFreedom,
-					CooperationCompetition = a.CooperationCompetition,
-					SecularismSpiritualism = a.SecularismSpiritualism,
-					ProgressivismTraditionalism = a.ProgressivismTraditionalism,
-					MonoculturalismMulticulturalism = a.MonoculturalismMulticulturalism,
-					Establishment = a.Establishment,
-					LowerPartyModifier = a.LowerPartyModiifer,
-					UpperPartyModifier = a.UpperPartyModifier,
-					LowerPartyAffinity = lowerAffinity.Value,
-					Cliques = a.AlignmentClique.Select(ac => ac.Clique.CliqueName).ToList()
-				};
-			}
+            foreach (var lowerAffinity in party.LowerPartyAffinity)
+            {
+                var a = lowerAffinity.Key;
+                var newEntry = new PopsimOverviewAlignment()
+                {
+                    Name = a.AlignmentName,
+                    FederalismCentralism = a.FederalismCentralism,
+                    DemocracyAuthority = a.DemocracyAuthority,
+                    GlobalismIsolationism = a.GlobalismIsolationism,
+                    MilitarismPacifism = a.MilitarismPacifism,
+                    SecurityFreedom = a.SecurityFreedom,
+                    CooperationCompetition = a.CooperationCompetition,
+                    SecularismSpiritualism = a.SecularismSpiritualism,
+                    ProgressivismTraditionalism = a.ProgressivismTraditionalism,
+                    MonoculturalismMulticulturalism = a.MonoculturalismMulticulturalism,
+                    Establishment = a.Establishment,
+                    LowerPartyModifier = a.LowerPartyModiifer,
+                    UpperPartyModifier = a.UpperPartyModifier,
+                    LowerPartyAffinity = lowerAffinity.Value,
+                    Cliques = a.AlignmentClique.Select(ac => ac.Clique.CliqueName).ToList()
+                };
+            }
 
-			foreach (var upperAffinity in party.UpperPartyAffinity)
-			{
-				var entry = overview.Alignments.SingleOrDefault(a => a.Name == upperAffinity.Key.AlignmentName);
-				entry.UpperPartyAffinity = upperAffinity.Value;
-			}
+            foreach (var upperAffinity in party.UpperPartyAffinity)
+            {
+                var entry = overview.Alignments.SingleOrDefault(a => a.Name == upperAffinity.Key.AlignmentName);
+                entry.UpperPartyAffinity = upperAffinity.Value;
+            }
 
-			return Ok(overview);
-		}
-	}
+            return Ok(overview);
+        }
+
+        [HttpGet("get-ggroups")]
+        [Authorize(AuthenticationSchemes = "Discord")]
+        public IActionResult GetPopsimGlobalGroups()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var discordUser = _context.DiscordUsers.SingleOrDefault(du => du.DiscordUserId == userId);
+
+            if (discordUser == null || !discordUser.IsAdmin)
+            {
+                return Unauthorized();
+            }
+
+            PopsimOverviewGlobalEthicGroup[] greups = new PopsimOverviewGlobalEthicGroup[_context.PopsimGlobalEthicGroups.Count()];
+            int x = 0;
+            foreach (PopsimGlobalEthicGroup ppeg in _context.PopsimGlobalEthicGroups)
+            {
+                greups[x] = new PopsimOverviewGlobalEthicGroup()
+                {
+                    Name = ppeg.PopsimGlobalEthicGroupName,
+                    FederalismCentralism = ppeg.FederalismCentralism,
+                    DemocracyAuthority = ppeg.DemocracyAuthority,
+                    GlobalismIsolationism = ppeg.GlobalismIsolationism,
+                    MilitarismPacifism = ppeg.MilitarismPacifism,
+                    SecurityFreedom = ppeg.SecurityFreedom,
+                    CooperationCompetition = ppeg.CooperationCompetition,
+                    SecularismSpiritualism = ppeg.SecularismSpiritualism,
+                    ProgressivismTraditionalism = ppeg.ProgressivismTraditionalism,
+                    MonoculturalismMulticulturalism = ppeg.MonoculturalismMulticulturalism,
+                    PopGroupEnlistment = ppeg.PartyEnlistmentModifier,
+                    PartyInvolvementFactor = ppeg.PartyInvolvementFactor,
+                    Radicalisation = ppeg.Radicalisation
+                };
+                x++;
+            }
+
+            return Ok(greups);
+        }
+
+        [HttpPost("set-group")]
+        [Authorize(AuthenticationSchemes = "Discord")]
+        public IActionResult SetPopsimGlobalGroup([FromBody] PopsimOverviewGlobalEthicGroup ppeg)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var discordUser = _context.DiscordUsers.SingleOrDefault(du => du.DiscordUserId == userId);
+
+            if (discordUser == null || !discordUser.IsAdmin)
+            {
+                return Unauthorized();
+            }
+
+            PopsimGlobalEthicGroup pp = _context.PopsimGlobalEthicGroups.FirstOrDefault(x => x.PopsimGlobalEthicGroupName == ppeg.Name);
+
+            if (pp is null)
+            {
+                return BadRequest();
+            }
+
+            pp.Radicalisation = ppeg.Radicalisation;
+            pp.PartyInvolvementFactor = ppeg.PartyInvolvementFactor;
+            pp.PartyEnlistmentModifier = ppeg.PopGroupEnlistment;
+            pp.FederalismCentralism = ppeg.FederalismCentralism;
+            pp.DemocracyAuthority = ppeg.DemocracyAuthority;
+            pp.GlobalismIsolationism = ppeg.GlobalismIsolationism;
+            pp.MilitarismPacifism = ppeg.MilitarismPacifism;
+            pp.SecurityFreedom = ppeg.SecurityFreedom;
+            pp.CooperationCompetition = ppeg.CooperationCompetition;
+            pp.SecularismSpiritualism = ppeg.SecularismSpiritualism;
+            pp.ProgressivismTraditionalism = ppeg.ProgressivismTraditionalism;
+            pp.MonoculturalismMulticulturalism = ppeg.MonoculturalismMulticulturalism;
+
+            _context.PopsimGlobalEthicGroups.Update(pp);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
 }
